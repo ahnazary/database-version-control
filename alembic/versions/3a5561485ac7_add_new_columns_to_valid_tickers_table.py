3a2b7054cf7dc22c5887e38590867e15c2160344
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-11-01 22:17:16 +0100

    Create new database version

---
+++ alembic/versions/3a5561485ac7_add_new_columns_to_valid_tickers_table.py
@@ -0,0 +1,51 @@
+"""add new columns to valid_tickers table
+
+Revision ID: 3a5561485ac7
+Revises: 5683f058b7e0
+Create Date: 2023-11-01 21:34:20.706895
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '3a5561485ac7'
+down_revision: Union[str, None] = '5683f058b7e0'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # add 8 new columns to valid_tickers table with names being:
+    #   - balance_sheet_annual_available
+    #   - cashflow_annual_available
+    #   - income_stmt_annual_available
+    #   - financials_annual_available
+    #   - balance_sheet_quarterly_available
+    #   - cashflow_quarterly_available
+    #   - income_stmt_quarterly_available
+    #   - financials_quarterly_available
+    op.add_column('valid_tickers', sa.Column('balance_sheet_annual_available', sa.Boolean(), nullable=True), schema='stocks')
+    op.add_column('valid_tickers', sa.Column('cashflow_annual_available', sa.Boolean(), nullable=True), schema='stocks')
+    op.add_column('valid_tickers', sa.Column('income_stmt_annual_available', sa.Boolean(), nullable=True), schema='stocks')
+    op.add_column('valid_tickers', sa.Column('financials_annual_available', sa.Boolean(), nullable=True), schema='stocks')
+    op.add_column('valid_tickers', sa.Column('balance_sheet_quarterly_available', sa.Boolean(), nullable=True), schema='stocks')
+    op.add_column('valid_tickers', sa.Column('cashflow_quarterly_available', sa.Boolean(), nullable=True), schema='stocks')
+    op.add_column('valid_tickers', sa.Column('income_stmt_quarterly_available', sa.Boolean(), nullable=True), schema='stocks')
+    op.add_column('valid_tickers', sa.Column('financials_quarterly_available', sa.Boolean(), nullable=True), schema='stocks')
+
+
+
+def downgrade() -> None:
+    # drop columns if they exist
+    op.drop_column('valid_tickers', 'balance_sheet_annual_available', schema='stocks')
+    op.drop_column('valid_tickers', 'cashflow_annual_available', schema='stocks')
+    op.drop_column('valid_tickers', 'income_stmt_annual_available', schema='stocks')
+    op.drop_column('valid_tickers', 'financials_annual_available', schema='stocks')
+    op.drop_column('valid_tickers', 'balance_sheet_quarterly_available', schema='stocks')
+    op.drop_column('valid_tickers', 'cashflow_quarterly_available', schema='stocks')
+    op.drop_column('valid_tickers', 'income_stmt_quarterly_available', schema='stocks')
+    op.drop_column('valid_tickers', 'financials_quarterly_available', schema='stocks')
