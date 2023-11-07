71661eefde748790e3188099c65262c960ca2818
ahnazary <ah.nazary.aghchemazary@gmail.com>
2023-11-07 21:53:41 +0100

    Fix linter

--- alembic/versions/3a5561485ac7_add_new_columns_to_valid_tickers_table.py
+++ alembic/versions/3a5561485ac7_add_new_columns_to_valid_tickers_table.py
@@ -7,9 +7,9 @@ Create Date: 2023-11-01 21:34:20.706895
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "3a5561485ac7"
